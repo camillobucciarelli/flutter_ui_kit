aafa43ef5d42a8064db149c125b1712aa12161fa
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-07-03 14:52:37 +0200

    fix

--- lib/form_fields/reactive_forms_fields/validators/dmy_date_validator.dart
+++ lib/form_fields/reactive_forms_fields/validators/dmy_date_validator.dart
@@ -1,10 +1,11 @@
+import 'package:intl/intl.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 class DmyDateValidator extends Validator {
   @override
   Map<String, dynamic>? validate(AbstractControl<dynamic> control) {
     try {
-      DateTime.parse(control.value!);
+      DateFormat('dd/MM/yyyy').parse(control.value!);
       return null;
     } catch (e) {
       return {ValidationMessage.mustMatch: ''};
