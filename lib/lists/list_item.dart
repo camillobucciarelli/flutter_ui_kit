70be9eb7e75c598327bc0a1f0d3457a6ba4a1b92
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-07-11 11:50:38 +0200

    added tappability on list item

--- lib/lists/list_item.dart
+++ lib/lists/list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_core_ui_kit/adaptive_container.dart';
 import 'package:flutter_core_ui_kit/theme/core_theme.dart';
 
 class ListItem extends StatefulWidget {
@@ -6,48 +7,69 @@ class ListItem extends StatefulWidget {
   final Widget? leading;
   final Widget? trailing;
   final EdgeInsets padding;
-  final bool? isShimmer;
+  final bool isShimmer;
   final double? height;
   final Color? backgroundColor;
+  final Color? backgroundSelectedColor;
   final VoidCallback? onTap;
+  final bool selectable;
+  final bool isSelected;
 
-  const ListItem(
-      {required this.title,
-      this.leading,
-      this.trailing,
-      this.isShimmer,
-      this.height,
-      this.backgroundColor,
-      this.onTap,
-      this.padding = const EdgeInsets.symmetric(horizontal: Dimens.SPACING_L)});
+  const ListItem({
+    required this.title,
+    this.leading,
+    this.trailing,
+    this.height,
+    this.backgroundColor,
+    this.backgroundSelectedColor,
+    this.onTap,
+    this.padding = const EdgeInsets.symmetric(horizontal: Dimens.SPACING_L),
+    this.isShimmer = false,
+    this.selectable = false,
+    this.isSelected = false,
+  }): assert(isShimmer && !selectable && !isSelected || !isShimmer);
 
   @override
   _ListItemState createState() => _ListItemState();
 }
 
 class _ListItemState extends State<ListItem> {
+  late double _factor;
   var _hover = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _toggleFactor();
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onHover: _onHover,
-      onTap: widget.onTap?.call,
-      child: AnimatedContainer(
-        duration: Durations.ANIMATED_TAP,
-        padding: widget.padding,
-        height: widget.height ?? Dimens.LIST_ITEM_HEIGHT,
-        alignment: Alignment.centerLeft,
-        decoration: _getDecoration(context),
-        child: Row(
-          children: [
-            if (widget.leading != null) ...[widget.leading!, const SizedBox(width: Dimens.SPACING_S)],
-            Expanded(child: widget.title),
-            if (widget.trailing != null) ...[
-              const SizedBox(width: Dimens.SPACING_L),
-              widget.trailing!,
-            ],
-          ],
+      onHover: widget.selectable ? _onHover : null,
+      onTap: _onTap,
+      onTapDown: (_) => _onTapDown(),
+      onTapCancel: _onTapCancel,
+      child: AdaptiveContainer(
+        child: Transform.scale(
+          scale: _factor,
+          child: AnimatedContainer(
+            duration: Durations.ANIMATED_TAP,
+            padding: widget.padding,
+            height: widget.height ?? Dimens.LIST_ITEM_HEIGHT,
+            alignment: Alignment.centerLeft,
+            decoration: _getDecoration(context),
+            child: Row(
+              children: [
+                if (widget.leading != null) ...[widget.leading!, const SizedBox(width: Dimens.SPACING_S)],
+                Expanded(child: widget.title),
+                if (widget.trailing != null) ...[
+                  const SizedBox(width: Dimens.SPACING_L),
+                  widget.trailing!,
+                ],
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -55,8 +77,43 @@ class _ListItemState extends State<ListItem> {
 
   void _onHover(bool hover) => setState(() => _hover = hover);
 
+  void _onTapCancel() {
+    if (!widget.selectable) return;
+    setState(() {
+      _factor = 1.0;
+    });
+  }
+
+  void _onTapDown() {
+    if (!widget.selectable) return;
+    setState(() {
+      _factor = 0.9;
+    });
+  }
+
+  void _onTap() async {
+    if (!widget.selectable) return;
+    await Future.delayed(Durations.BUTTON_TAP);
+    setState(_toggleFactor);
+    widget.onTap?.call();
+  }
+
+  void _toggleFactor() {
+    if (widget.isSelected) {
+      _factor = 0.9;
+    } else {
+      _factor = 1.0;
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant ListItem oldWidget) {
+    if (oldWidget.isSelected != widget.isSelected) setState(_toggleFactor);
+    super.didUpdateWidget(oldWidget);
+  }
+
   BoxDecoration _getDecoration(BuildContext context) {
-    if (widget.isShimmer ?? false) {
+    if (widget.isShimmer) {
       return BoxDecoration(
         borderRadius: BorderRadius.circular(Dimens.RADIUS_L),
         border: Border.all(color: ThemeColors.of(context).primaryLight),
@@ -65,7 +122,9 @@ class _ListItemState extends State<ListItem> {
 
     return BoxDecoration(
       borderRadius: BorderRadius.circular(Dimens.RADIUS_L),
-      color: widget.backgroundColor ?? ThemeColors.of(context).primaryLight,
+      color: widget.isSelected
+          ? widget.backgroundSelectedColor ?? ThemeColors.of(context).primary
+          : widget.backgroundColor ?? ThemeColors.of(context).primaryLight,
       boxShadow: [
         if (_hover)
           BoxShadow(
