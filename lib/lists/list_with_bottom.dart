7d63240cb256f2bf3b00b36446d0e201c3e1652d
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-08-01 08:50:01 +0200

    added list with bottom

--- lib/lists/list_with_bottom.dart
+++ lib/lists/list_with_bottom.dart
@@ -4,8 +4,9 @@ import 'package:flutter_core_ui_kit/flutter_core_ui_kit.dart';
 class ListWithBottom extends StatelessWidget {
   final List<Widget> children;
   final Widget bottom;
+  final double bottomSpacing;
 
-  const ListWithBottom({required this.children, required this.bottom});
+  const ListWithBottom({required this.children, required this.bottom, this.bottomSpacing = Dimens.SPACING_XXL});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +16,7 @@ class ListWithBottom extends StatelessWidget {
         SliverList(
           delegate: SliverChildListDelegate([
             ...children,
-            const SizedBox(height: Dimens.SPACING_XXL),
+            SizedBox(height: bottomSpacing),
           ]),
         ),
         SliverFillRemaining(
