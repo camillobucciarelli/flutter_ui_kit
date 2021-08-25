23e1583fe7077d88396597b6ea94a516621591b4
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2021-08-25 12:17:44 +0200

    modify customized_app_bar.dart

--- lib/customized_app_bar.dart
+++ lib/customized_app_bar.dart
@@ -8,19 +8,25 @@ import 'package:flutter_core_ui_kit/theme/core_theme.dart';
 import 'buttons/button.dart';
 
 class CustomizedAppBar extends StatefulWidget implements PreferredSizeWidget {
-  final String title;
+  final String? title;
+  final Widget? titleWidget;
+  final bool centerTitle;
   final List<Widget>? actions;
   final Widget? leading;
   final double fadeHeight;
+  final double appBarHeight;
   final ScrollController? scrollController;
 
   const CustomizedAppBar({
-    this.title = '',
+    this.title,
+    this.titleWidget,
+    this.centerTitle = true,
     this.leading,
     this.actions,
     this.scrollController,
     this.fadeHeight = 0,
-  });
+    this.appBarHeight = Dimens.APP_BAR_HEIGHT,
+  }): assert((title != null && titleWidget == null) || (title == null && titleWidget != null));
 
   @override
   _CustomizedAppBarState createState() => _CustomizedAppBarState();
@@ -69,10 +75,11 @@ class _CustomizedAppBarState extends State<CustomizedAppBar> {
           filter: Dimens.blurredFilter,
           child: AppBar(
             title: _title,
+            centerTitle: widget.centerTitle,
             leadingWidth: Dimens.SPACING_M * 2 + Dimens.ICON_BUTTON_SIZE,
             leading: widget.leading ?? CustomizedAppBar.leadingButton(context),
             elevation: 0,
-            toolbarHeight: Dimens.APP_BAR_HEIGHT,
+            toolbarHeight: widget.appBarHeight,
             actions: [...widget.actions ?? [], const SizedBox(width: Dimens.SPACING_M)],
           ),
         ),
@@ -81,20 +88,24 @@ class _CustomizedAppBarState extends State<CustomizedAppBar> {
   }
 
   Widget get _title {
+    Widget _titleWidget() {
+      if(widget.title != null) {
+        return Text(
+          widget.title!,
+          style: Theme.of(context).textTheme.appBar,
+        );
+      }
+      return widget.titleWidget!;
+    }
+
     if (widget.fadeHeight == 0) {
-      return Text(
-        widget.title,
-        style: Theme.of(context).textTheme.appBar,
-      );
+      return _titleWidget();
     }
 
     return AnimatedOpacity(
       duration: const Duration(milliseconds: 16),
       opacity: _titleOpacity,
-      child: Text(
-        widget.title,
-        style: Theme.of(context).textTheme.appBar,
-      ),
+      child: _titleWidget(),
     );
   }
 }
